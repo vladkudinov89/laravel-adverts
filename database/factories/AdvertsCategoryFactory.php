0ba4281f209a0aa1135e73a0276bee1393c09590
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-10-23 01:21:10 +0300

    Add migration , factory , seeder, migration , model Advert_Category.

---
+++ database/factories/AdvertsCategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Entity\Adverts\Category;
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->unique()->name,
+        'slug' => $faker->unique()->slug(2),
+        'parent_id' => null,
+    ];
+});
